12bd53c14b28cd024b71a233261b6f945df8d464
mcajay01 <82970644+mcajay01@users.noreply.github.com>
2022-02-22 09:31:11 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,9 @@
-$DownloadPath = "https://github.com/LesSolutionsOneSky/Control/blob/main/Control_Agent.msi"
-$SoftwarePath = "C:\Temp\Control_Agent.msi"
+$DownloadPath = "https://screenconnect.aidepc.ca:4443/Bin/One-Sky.ClientSetup.exe?h=screenconnect.aidepc.ca&p=8041&k=BgIAAACkAABSU0ExAAgAAAEAAQDVaolrsK5KPjWzlyXPqf2Tgu54EBdrxl5sr55VJawQru7JFBNcp1ZwyDia2xnaBRsUimo%2BQ3O78dLnqa94ADRnD1hUr%2FBkUaKUd9%2ByU0ZwIqnS8je%2Br7YEia9MdWrPyukD3wM5zy7D67%2BTH8a%2Bi%2BUyVtR2DQaWGGoIg9bnlR6r2gvGUElchkHx7VCKgjnys8CvTmmrfrcYOTFxGtnfmIb11x800zxWeotLMcpH9Mpo5l17GbqVAjyovs%2FusQyvYyTYyXjleFeIymOooos%2B8q4VYe2XyVs%2F9xWUA48dQAn1LqZ5C%2BNGX%2BT4vC3CNSw7BUGw%2FHHrudTMSiMZzySFK%2Fa5&e=Access&y=Guest&t=&c=A%20Verifier&c=&c=&c=&c=&c=&c=&c="
+$SoftwarePath = "C:\Temp\Control_Agent.exe"
 
     Write-Host "Downloading from: $($DownloadPath)"
     Write-Host "Downloading to:   $($SoftwarePath)"
         $WebClient = New-Object System.Net.WebClient
         $WebClient.DownloadFile($DownloadPath, $SoftwarePath)
     Write-Host "Download Complete"
-
-Start-Process MSIEXEC.exe -ArgumentList "/I $SoftwarePath /quiet /qn /norestart" -Wait -Verb RunAs -PassThru
+Start-Process -FilePath $SoftwarePath -ArgumentList "/quiet /norestart" -Wait -Verb RunAs -PassThru
